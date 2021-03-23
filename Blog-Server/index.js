eaec05381d739dc719b25d6abc68bf326c1ab861
gavindang2911 <hoaidang2999@gmail.com>
2021-03-23 07:00:34 -0700

    Added heroku

--- Blog-Server/index.js
+++ Blog-Server/index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Port = process.env.port || 5000;
+
 const app = express();
 
 
-mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
+
+mongoose.connect('mongodb+srv://BlogUser:gavin112233a@cluster0.zugje.mongodb.net/appDB?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}
+);
 const connection = mongoose.connection;
 connection.once("open",()=>{
     console.log("MongoDb connected")
 })
 
 //Middleware
+app.use(express.json());
 const userRoute = require("./routes/user");
 app.use("/user", userRoute);
 
